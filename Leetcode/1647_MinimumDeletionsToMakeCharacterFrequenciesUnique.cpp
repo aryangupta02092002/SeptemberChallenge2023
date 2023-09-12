f13c741fb18a5b05a556be9c4f2839e327e21275
Aryan Gupta <72204017+aryangupta02092002@users.noreply.github.com>
2023-09-12 18:09:02 +0530

    Sept 12 - String | Frequency Table

---
+++ Leetcode/1647_MinimumDeletionsToMakeCharacterFrequenciesUnique.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        vector<int> freq(26, 0);
+        for(int i=0; i<s.size(); i++){
+            freq[s[i]-'a']++;
+        }
+        
+        sort(freq.begin(), freq.end());
+        int deleted = 0;
+        for(int i=24; i>=0; i--){
+            if(freq[i] == 0){
+                break;
+            }
+            if(freq[i] >= freq[i+1]){
+                int prev = freq[i];
+                freq[i] = max(0, freq[i+1]-1);
+                deleted += prev - freq[i];
+            }
+            
+        }
+        return deleted;
+        
+    }
+};
